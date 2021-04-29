0093251d11f734610f78fbfb65c12f5715db0f48
enrico bianco <biancoenrico71@gmail.com>
2021-04-29 16:19:32 +0200

    aggiunta ricerca combinata

--- js/script.js
+++ js/script.js
@@ -1,31 +1,69 @@
 var app = new Vue({
     el: '#root',
     data: {
-        result:[],
+        resultfilm:[],
+        resultSeries:[],
         search:''
     },
     methods: {
+        find(){
+            this.searchFilm(),
+            this.searchTv()
+        },
         searchFilm(){
+        
             axios
-                .get('https://api.themoviedb.org/3/search/movie?api_key=08103b63752bfcc4876a0aec5cc20e97',
+                .get('https://api.themoviedb.org/3/search/movie?',
+                
                 {
                     params:{
+                        api_key: '08103b63752bfcc4876a0aec5cc20e97',
                         query: this.search,
-                        // backdrop_path: this.result.backdrop_path
                     }
 
                 })
                 
+                
+                
                 .then((response)=>{
                     list = response.data;
-                    this.result = list.results;
+                    this.resultfilm = list.results;
+                })  
+
+        },
+        searchTv(){
+        
+
+            axios
+                .get('https://api.themoviedb.org/3/search/tv?',
+                
+                {
+                    params:{
+                        api_key: '08103b63752bfcc4876a0aec5cc20e97',
+                        query: this.search,
+                    }
+
                 })
+                
+                
+                
+                .then((response)=>{
+                    list = response.data;
+                    this.resultSeries = list.results;
+                }) 
         }
+        
         
     },
     mounted(){
         axios
-            .get('https://api.themoviedb.org/3/search/movie?api_key=08103b63752bfcc4876a0aec5cc20e97')
+            .get('https://api.themoviedb.org/3/search/movie?',
+                'https://api.themoviedb.org/3/search/tv?',
+            {
+                params:{
+                    api_key: '08103b63752bfcc4876a0aec5cc20e97',
+                }
+            })
             .then((response)=>{
                 console.log(response.data);
                 let list = response.data;
