88625a1f55a12446c4baabc296613fa5714a719b
enrico bianco <biancoenrico71@gmail.com>
2021-04-30 12:43:30 +0200

    aggiunta struttura html e stelline

--- js/script.js
+++ js/script.js
@@ -2,43 +2,31 @@ var app = new Vue({
     el: '#root',
     data: {
         resultMulti:[],
-        search:'back to the future',
-        stars:4,
-        coloredStars:[]
+        search:'ciao',
     },
     methods: {
         
-        
+        getVote(vote){
+            const stars = Math.ceil(vote / 2);
+            return stars;
+        },
         searchMulti(){
         
             axios
-                .get('https://api.themoviedb.org/3/search/multi?',
-                {
-                    params:{
-                        api_key:'08103b63752bfcc4876a0aec5cc20e97',
-                        query: this.search,
-                    }
+            .get('https://api.themoviedb.org/3/search/multi?',
+            {
+                params:{
+                    api_key:'08103b63752bfcc4876a0aec5cc20e97',
+                    query: this.search,
+                }
+            })
 
-                })
-                
-                .then((response)=>{
-                    let list = response.data;
-                    this.resultMulti = list.results;
-                    
-                    this.resultMulti.forEach((element)=>{
-                        element.vote_average = Math.ceil(element.vote_average / 2).toFixed();
-                        this.stars = element.vote_average
-                        console.log(this.stars);
-                    })
-
-                    for(let i = 0; i == this.stars; i++){
-                        this.coloredStars.push('star')
-                    }
-            })  
+            .then((response)=>{
+                let list = response.data;
+                this.resultMulti = list.results;
             this.search = '';
-        },
-       
-        
+        })  
+        }, 
     },
     mounted(){
        
@@ -54,18 +42,10 @@ var app = new Vue({
             .then((response)=>{
                 let list = response.data;
                 this.resultMulti = list.results;
-                
-                this.resultMulti.forEach((element)=>{
-                element.vote_average = Math.ceil(element.vote_average / 2).toFixed();
-                
-                })
-                console.log(this.stars);
-                for(let i = 0; i <= this.stars; i++){
-                    this.coloredStars.push(i)
-                    console.log(this.coloredStars);
-                }
+                getVote(this.resultMulti.vote_average)
 
-        })
-        this.search = '';
+                
+            this.search = '';
+        })     
     },
 })
