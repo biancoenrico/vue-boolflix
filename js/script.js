ec7dd9f7d381b73e0b732b6c8cf3047cd1ab680f
Enrico Bianco <biancoenrico71@gmail.com>
2021-07-07 17:10:16 +0200

    generi e cast

--- js/script.js
+++ js/script.js
@@ -1,102 +1,173 @@
 var app = new Vue({
     el: '#root',
     data: {
-        // ricerca tramite searchbar
-        resultMulti:[],
-        search:'',
-        // fine ricerca tramite searchbar
-
-        // ricerca tramite genere
-        genreArray:[],
-        genreSelect:[''],
-        selectedGen:'',
-        // fine ricerca tramite genere
-        
+        resultMulti: [],
+        resultSeries: [],
+        resultMovie: [],
+        search: '',
+        id_search: [],
+        genres: '',
+        genreSelect: '',
     },
     methods: {
-
-        // ricerca serie tv/film tramite searchbar
-        searchMulti(){
-            axios
-            .get('https://api.themoviedb.org/3/search/multi?',
-            {
-                params:{
-                    api_key:'08103b63752bfcc4876a0aec5cc20e97',
-                    query: this.search,
-                    genre_ids: this.selectedGen[this.genreArray]
-                }
-            })
-
-            .then((response)=>{
-                let list = response.data;
-                this.resultMulti = list.results;
-                this.search = '';
-            })   
-        }, 
-        // fine ricerca serie tv/film tramite searchbar
-
-        // funzione per prendere i generi dei film
-        gen(){
-            axios
-                .get('https://api.themoviedb.org/3/genre/movie/list?',
-                {
-                    params:{
-                        api_key:'08103b63752bfcc4876a0aec5cc20e97',
-                        
-                    }
-                })
-                .then((response)=>{
-                    let list = response.data;
-                    this.genreArray = list.genres;
-                })
-        },
-        // fine funzione per prendere i generi dei film
-
-        // test
-        searchAll(){
-            this.gen();
-            this.searchMulti();
-        },
-        // fine test
-
-        // funzione per cambiare il voto in intero da 1 a 5
-        getVote(vote){
+        getVote(vote) {
             const stars = Math.ceil(vote / 2);
             return stars;
         },
-        // fine funzione per cambiare il voto in intero da 1 a 5
+
+        searchFilms() {
+            axios
+                .get('https://api.themoviedb.org/3/search/movie', {
+                    params: {
+                        api_key: '08103b63752bfcc4876a0aec5cc20e97',
+                        query: this.search,
+                    },
+                })
+
+                .then((response) => {
+                    this.resultMulti = [];
+                    const resultMovie = response.data.results;
+                    this.search = '';
+
+                    resultMovie.forEach((element) => {
+                        axios
+                            .get(
+                                'https://api.themoviedb.org/3/movie/' +
+                                    element.id,
+                                {
+                                    params: {
+                                        api_key:
+                                            'a21d6a53ae3ba4432e6ec0b5967e1ce3',
+                                        append_to_response: 'credits',
+                                    },
+                                }
+                            )
+                            .then((response) => {
+                                element.genre = response.data.genres.slice(
+                                    0,
+                                    2
+                                );
+                                element.cast = response.data.credits.cast.slice(
+                                    0,
+                                    5
+                                );
+                                Vue.set(
+                                    element,
+                                    'genre',
+                                    response.data.genres.slice(0, 2)
+                                );
+
+                                Vue.set(
+                                    element,
+                                    'cast',
+                                    response.data.credits.cast.slice(0, 5)
+                                );
+
+                                this.resultMovie.push(element);
+                                console.log(this.resultMovie);
+                            });
+                        this.resultMovie = [];
+                    });
+                });
+
+            axios;
+            axios
+                .get('https://api.themoviedb.org/3/search/tv', {
+                    params: {
+                        api_key: '08103b63752bfcc4876a0aec5cc20e97',
+                        query: this.search,
+                    },
+                })
+
+                .then((response) => {
+                    this.resultMulti = [];
+                    const resultTv = response.data.results;
+                    this.search = '';
+
+                    resultTv.forEach((element) => {
+                        axios
+                            .get(
+                                'https://api.themoviedb.org/3/tv/' + element.id,
+                                {
+                                    params: {
+                                        api_key:
+                                            'a21d6a53ae3ba4432e6ec0b5967e1ce3',
+                                        append_to_response: 'credits',
+                                    },
+                                }
+                            )
+                            .then((response) => {
+                                element.genre = response.data.genres.slice(
+                                    0,
+                                    2
+                                );
+                                element.cast = response.data.credits.cast.slice(
+                                    0,
+                                    5
+                                );
+                                Vue.set(
+                                    element,
+                                    'genre',
+                                    response.data.genres.slice(0, 2)
+                                );
+
+                                Vue.set(
+                                    element,
+                                    'cast',
+                                    response.data.credits.cast.slice(0, 5)
+                                );
+
+                                this.resultSeries.push(element);
+                                console.log(this.resultSeries);
+                            });
+                        this.resultSeries = [];
+                    });
+                });
+        },
+
+        showGen() {            
+            
+        },
     },
-    
-    mounted(){
+
+    mounted() {
         // per mostrare i film piu popolari appena aperta la pagina
         axios
-            .get('https://api.themoviedb.org/3/movie/popular?',
-            {
-                params:{
-                    api_key:'08103b63752bfcc4876a0aec5cc20e97',
-                }
+            .get('https://api.themoviedb.org/3/movie/popular?', {
+                params: {
+                    api_key: '08103b63752bfcc4876a0aec5cc20e97',
+                },
             })
 
-            .then((response)=>{
-                let list = response.data;
-                this.resultMulti = list.results;
+            .then((response) => {
+                this.resultMulti = response.data.results;
 
-            this.search = '';
-        }) 
+                this.search = '';
+            });
+
         axios
             // per mostrare la select appena caricata la pagina
-            .get('https://api.themoviedb.org/3/genre/movie/list?',
-            {
-                params:{
-                    api_key:'08103b63752bfcc4876a0aec5cc20e97',
-                    query: this.genreSelect,
-                }
+            .get('https://api.themoviedb.org/3/genre/movie/list?', {
+                params: {
+                    api_key: '08103b63752bfcc4876a0aec5cc20e97',
+                },
             })
-            .then((response)=>{
-                let list = response.data;
-                this.genreArray = list.genres;
-            });
-                
-    },
 
-})
+            .then((response) => {
+                this.genres = response.data.genres;
+                console.log(this.genres);
+            });
+
+        axios
+            .get('https://api.themoviedb.org/3/genre/tv/list?', {
+                params: {
+                    api_key: '08103b63752bfcc4876a0aec5cc20e97',
+                },
+            })
+
+            .then((response) => {
+                this.genres.push(response.data.genres);
+                console.log(this.genres);
+            });
+    },
+});
